5b57986dbbe79732e5001d40bbc18adee2abf073
anvesh1122 <chilakaraju.anvesh7799@gmail.com>
2019-09-14 13:02:13 +0530

    third commit

--- src/ChromeDriverlaunch.java
+++ src/ChromeDriverlaunch.java
@@ -26,6 +26,7 @@ public static void main(String[] args) throws InterruptedException, IOException
 	WebElement pass=d.findElementByXPath("//input[@type='password']");
 	pass.sendKeys("9618989578");
 	Thread.sleep(4000);
+	System.out.println("ignore this it just git reference");
 	
 	WebElement Show=d.findElementByXPath("//span[@class='Ce1Y1c']");
 	Show.click();
